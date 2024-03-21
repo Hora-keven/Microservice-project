614e6982ec850f7fb0f2ba5eea639d7eb854b602
hora-keven <kevensantos993@gmail.com>
2024-03-21 11:08:58 -0300

    feat: feign clients added in department-service application

--- department-service/src/main/java/com/study/serviceregistry/DepartmentServiceApplication.java
+++ department-service/src/main/java/com/study/serviceregistry/DepartmentServiceApplication.java
@@ -3,9 +3,11 @@ package com.study.serviceregistry;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.openfeign.EnableFeignClients;
 
 @SpringBootApplication
 @EnableDiscoveryClient
+@EnableFeignClients
 public class DepartmentServiceApplication {
 
 	public static void main(String[] args) {
