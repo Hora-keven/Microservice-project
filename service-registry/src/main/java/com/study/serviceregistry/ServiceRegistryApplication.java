c0a5405d5073ffedf48badd56cc43c7df62a2fe9
hora-keven <kevensantos993@gmail.com>
2024-03-22 11:25:16 -0300

    feat: clean up the code

--- service-registry/src/main/java/com/study/serviceregistry/ServiceRegistryApplication.java
+++ service-registry/src/main/java/com/study/serviceregistry/ServiceRegistryApplication.java
@@ -1,5 +1,4 @@
 package com.study.serviceregistry;
-
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
