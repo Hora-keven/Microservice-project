a98c75410796aaa8dd5d6a762440176fb32397d7
hora-keven <kevensantos993@gmail.com>
2024-03-22 11:25:09 -0300

    feat: clean up the code

--- employee-service/src/main/java/com/employeeservice/controller/EmployeeController.java
+++ employee-service/src/main/java/com/employeeservice/controller/EmployeeController.java
@@ -1,7 +1,6 @@
 package com.employeeservice.controller;
 
 import java.util.List;
-
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -13,7 +12,6 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
-
 import com.employeeservice.model.Employee;
 import com.employeeservice.repository.EmployeeRepository;
 
