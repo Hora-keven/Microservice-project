d7d518122dfba3bf2a703d442872bc622aee98fd
hora-keven <kevensantos993@gmail.com>
2024-03-22 11:24:58 -0300

    feat: clean up the code

--- config-server/src/main/java/com/study/configserver/ConfigServerApplication.java
+++ config-server/src/main/java/com/study/configserver/ConfigServerApplication.java
@@ -1,5 +1,4 @@
 package com.study.configserver;
-
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.config.server.EnableConfigServer;
